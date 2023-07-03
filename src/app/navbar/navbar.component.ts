5467b5e0bf5c43cb559bee802e53de627ac3c367
jesuspinamunoz <jesuspm1994@gmail.com>
2023-07-03 08:41:56 +0000

    añadir barra objetivos al panel

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { BackConnService } from 'src/app/back-conn.service'
 import { HttpErrorResponse } from '@angular/common/http';
 import { constants } from 'src/app/constants';
+import { Chart, BarController, CategoryScale } from 'chart.js';
 
 document.documentElement.style.setProperty('--navMenuWidth', constants.navMenuWidth);
 
@@ -13,10 +14,81 @@ document.documentElement.style.setProperty('--navMenuWidth', constants.navMenuWi
 })
 export class NavbarComponent implements OnInit {
   league: string = '';
+  
+  progressChart: any = {};
 
   constructor(private service: BackConnService, private router: Router) { }
 
   ngOnInit(): void {
+    Chart.register(BarController, CategoryScale);
+
+    this.progressChart = new Chart("progressChart", {
+      type: 'bar',
+      data: {
+        labels: ['Progress'],
+        datasets: [
+        {
+          data: [80],
+          backgroundColor: 'rgba(3, 252, 22, 0.5)',
+          borderColor: [
+            'rgba(0, 0, 0, 1)',
+          ],
+          borderWidth: 2,
+          borderSkipped: 'right',
+
+        },
+        {
+          data: [100],
+          backgroundColor: 'rgba(217, 217, 217, 1)',
+          borderColor: [
+            'rgba(0, 0, 0, 1)',
+          ],
+          borderWidth: 2,
+          
+          borderSkipped: false,
+        }      
+      ]
+      },
+      options: {
+        plugins: {
+          legend:{
+            display:false,
+          },
+        },
+        indexAxis: 'y',
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            // stacked: true,
+            display: false,
+            min: 0,
+            max: 100,
+            ticks: {
+              stepSize: 10
+            }
+          },
+          y: {
+            stacked: true,
+            display: false
+          }
+        },
+        elements: {
+          bar: {
+            borderRadius: {
+              topLeft: 10,
+              topRight: 10,
+              bottomLeft: 10,
+              bottomRight: 10
+            }
+          }
+        }
+      }
+    });
+    
+    
+
+
   }
 
   onSelected(value: string): void {
